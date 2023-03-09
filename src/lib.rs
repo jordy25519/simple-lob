707b5a4d587071d90fdfbbe422676bd99dd0b558
holygits <beauchjord@gmail.com>
2023-03-09 14:41:27 +0000

    polymorphic version

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,27 @@ pub trait LOB {
     ) -> Result<Vec<Fill>, Self::Error>;
 }
 
+pub trait TryFill {
+    type Opposite: TryFill;
+    /// Whether the order's value is zero
+    fn is_zero(&self) -> bool;
+    /// Try fill this order with `other`
+    fn try_fill(&mut self, other: &mut Self::Opposite) -> Option<(Fill, Fill)>;
+}
+
 #[derive(PartialEq, PartialOrd, Clone, Debug)]
 pub enum OrderSide {
     Buy,
     Sell,
+}
+
+impl OrderSide {
+    pub fn opposite(&self) -> Self {
+        match self {
+            Self::Buy => Self::Sell,
+            Self::Sell => Self::Buy,
+        }
+    }
 }
 
 // An event denoting a matched order
@@ -43,25 +60,63 @@ impl Fill {
 }
 
 /// Note: field declaration order is important for derived sort implementation
-#[derive(PartialEq, Clone, Debug)]
+#[derive(PartialEq, Clone, Debug, Default)]
 struct LimitOrder {
     price: f32,
     nonce: u64,
-    // TODO: this could be handled by the type system or inferred
-    // TODO: the sorting is reversed for each side of the book
-    side: OrderSide,
     amount: u32,
     trader_id: u32,
+}
+#[derive(PartialEq, Clone, Debug, Default)]
+struct BuyLimitOrder(LimitOrder);
+
+impl From<LimitOrder> for BuyLimitOrder {
+    fn from(f: LimitOrder) -> Self {
+        Self(f)
+    }
+}
+
+#[derive(PartialEq, Clone, Debug, Default)]
+struct SellLimitOrder(LimitOrder);
+
+impl From<LimitOrder> for SellLimitOrder {
+    fn from(f: LimitOrder) -> Self {
+        Self(f)
+    }
+}
+
+impl TryFill for BuyLimitOrder {
+    type Opposite = SellLimitOrder;
+    #[inline(always)]
+    fn is_zero(&self) -> bool {
+        self.0.amount == 0
+    }
+    fn try_fill(&mut self, other: &mut Self::Opposite) -> Option<(Fill, Fill)> {
+        if self.0.price >= other.0.price {
+            self.0.try_fill(&mut other.0, OrderSide::Buy)
+        } else {
+            None
+        }
+    }
+}
+
+impl TryFill for SellLimitOrder {
+    type Opposite = BuyLimitOrder;
+    fn is_zero(&self) -> bool {
+        self.0.amount == 0
+    }
+    fn try_fill(&mut self, other: &mut Self::Opposite) -> Option<(Fill, Fill)> {
+        if self.0.price <= other.0.price {
+            self.0.try_fill(&mut other.0, OrderSide::Sell)
+        } else {
+            None
+        }
+    }
 }
 
 impl LimitOrder {
     /// Try fill `self` with `other`, returns the resulting `Fill` events if any
-    fn try_fill(&mut self, other: &mut LimitOrder) -> Option<(Fill, Fill)> {
-        if (self.side == OrderSide::Buy && self.price < other.price)
-            || (self.side == OrderSide::Sell && self.price > other.price)
-        {
-            return None;
-        }
+    fn try_fill(&mut self, other: &mut LimitOrder, side: OrderSide) -> Option<(Fill, Fill)> {
         let fill_amount = match self.amount.cmp(&other.amount) {
             Ordering::Equal | Ordering::Less => {
                 let amount = self.amount;
@@ -81,14 +136,14 @@ impl LimitOrder {
             Fill::new(
                 fill_amount,
                 self.price,
-                self.side.clone(),
+                side.clone(),
                 self.trader_id,
                 other.trader_id,
             ),
             Fill::new(
                 fill_amount,
                 self.price,
-                other.side.clone(),
+                side.opposite(),
                 other.trader_id,
                 self.trader_id,
             ),
@@ -96,43 +151,54 @@ impl LimitOrder {
     }
 }
 
-impl PartialOrd for LimitOrder {
+impl PartialOrd for BuyLimitOrder {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match self.price.total_cmp(&other.price) {
-            Ordering::Equal => self.nonce.partial_cmp(&other.nonce),
-            Ordering::Greater => match self.side {
-                OrderSide::Buy => Some(Ordering::Less),
-                OrderSide::Sell => Some(Ordering::Greater),
-            },
-            Ordering::Less => match self.side {
-                OrderSide::Buy => Some(Ordering::Greater),
-                OrderSide::Sell => Some(Ordering::Less),
-            },
+        match self.0.price.total_cmp(&other.0.price) {
+            Ordering::Equal => self.0.nonce.partial_cmp(&other.0.nonce),
+            Ordering::Greater => Some(Ordering::Less),
+            Ordering::Less => Some(Ordering::Greater),
         }
     }
 }
 
-impl Ord for LimitOrder {
+impl Ord for BuyLimitOrder {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.partial_cmp(other)
             .expect("only valid floats are given")
     }
 }
 
-impl Eq for LimitOrder {}
+impl PartialOrd for SellLimitOrder {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.0.price.total_cmp(&other.0.price) {
+            Ordering::Equal => self.0.nonce.partial_cmp(&other.0.nonce),
+            order => Some(order),
+        }
+    }
+}
+
+impl Ord for SellLimitOrder {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other)
+            .expect("only valid floats are given")
+    }
+}
+
+impl Eq for SellLimitOrder {}
+impl Eq for BuyLimitOrder {}
 
 #[derive(Default, Debug)]
-struct OrderBook(VecDeque<LimitOrder>);
+struct OrderBook<T: Clone + Ord + TryFill>(VecDeque<T>);
 
-impl OrderBook {
-    pub fn front(&self) -> Option<&LimitOrder> {
+impl<T: Clone + Ord + TryFill> OrderBook<T> {
+    pub fn front(&self) -> Option<&T> {
         self.0.front()
     }
     pub fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
     /// Insert an order into the book at the correct location
-    pub fn insert_order(&mut self, order: &LimitOrder) -> Result<(), ()> {
+    pub fn insert_order(&mut self, order: &T) -> Result<(), ()> {
         if let Err(idx) = self.0.binary_search(order) {
             self.0.insert(idx, order.clone());
             Ok(())
@@ -144,8 +210,8 @@ impl OrderBook {
     /// Returning fills and remaining unfilled order if any
     pub fn submit_order<'a>(
         &mut self,
-        order: &'a mut LimitOrder,
-    ) -> (Vec<Fill>, Option<&'a LimitOrder>) {
+        order: &'a mut T::Opposite,
+    ) -> (Vec<Fill>, Option<&'a T::Opposite>) {
         // try add the order to the book absorbing any resting liquidity
         let mut fills = Vec::<Fill>::default();
         let mut remove_count = 0;
@@ -153,13 +219,13 @@ impl OrderBook {
             if let Some((fill_0, fill_1)) = resting_order.try_fill(order) {
                 fills.push(fill_0);
                 fills.push(fill_1);
-                if resting_order.amount == 0 {
+                if resting_order.is_zero() {
                     remove_count += 1;
                 }
             } else {
                 break;
             }
-            if order.amount == 0 {
+            if order.is_zero() {
                 break;
             }
         }
@@ -169,10 +235,10 @@ impl OrderBook {
             let _ = self.0.drain(0..remove_count);
         }
 
-        if order.amount > 0 {
-            (fills, Some(order))
-        } else {
+        if order.is_zero() {
             (fills, None)
+        } else {
+            (fills, Some(order))
         }
     }
 }
@@ -181,8 +247,8 @@ impl OrderBook {
 pub struct Market {
     /// Order nonce
     nonce: u64,
-    buys: OrderBook,
-    sells: OrderBook,
+    buys: OrderBook<BuyLimitOrder>,
+    sells: OrderBook<SellLimitOrder>,
 }
 
 impl LOB for Market {
@@ -194,16 +260,20 @@ impl LOB for Market {
         price: f32,
         side: OrderSide,
     ) -> Result<Vec<Fill>, Self::Error> {
-        let mut order = LimitOrder {
+        if amount == 0 {
+            return Ok(vec![]);
+        }
+
+        let order = LimitOrder {
             price,
             amount,
             trader_id,
-            side: side.clone(),
             nonce: self.nonce,
         };
 
         let fills = match side {
             OrderSide::Buy => {
+                let mut order = order.into();
                 let (fills, unfilled) = self.sells.submit_order(&mut order);
                 if let Some(unfilled) = unfilled {
                     self.buys
@@ -213,6 +283,7 @@ impl LOB for Market {
                 fills
             }
             OrderSide::Sell => {
+                let mut order = order.into();
                 let (fills, unfilled) = self.buys.submit_order(&mut order);
                 if let Some(unfilled) = unfilled {
                     self.sells
@@ -230,32 +301,32 @@ impl LOB for Market {
 
 #[cfg(test)]
 pub mod tests {
-    use crate::{Fill, LimitOrder, Market, OrderSide, LOB};
+    use crate::{BuyLimitOrder, Fill, LimitOrder, Market, OrderSide, SellLimitOrder, LOB};
 
     #[test]
     fn orders_sort_by_price_then_nonce() {
-        let mut orders = vec![
+        let mut orders: Vec<BuyLimitOrder> = vec![
             LimitOrder {
                 trader_id: 1,
                 nonce: 2,
                 price: 2.0,
                 amount: 1,
-                side: OrderSide::Buy,
-            },
+            }
+            .into(),
             LimitOrder {
                 trader_id: 1,
                 nonce: 1,
                 price: 2.0,
                 amount: 1,
-                side: OrderSide::Buy,
-            },
+            }
+            .into(),
             LimitOrder {
                 trader_id: 1,
                 nonce: 3,
                 price: 1.0,
                 amount: 1,
-                side: OrderSide::Buy,
-            },
+            }
+            .into(),
         ];
         orders.sort();
 
@@ -267,49 +338,50 @@ pub mod tests {
                     nonce: 1,
                     price: 2.0,
                     amount: 1,
-                    side: OrderSide::Buy,
-                },
+                }
+                .into(),
                 LimitOrder {
                     trader_id: 1,
                     nonce: 2,
                     price: 2.0,
                     amount: 1,
-                    side: OrderSide::Buy,
-                },
+                }
+                .into(),
                 LimitOrder {
                     trader_id: 1,
                     nonce: 3,
                     price: 1.0,
                     amount: 1,
-                    side: OrderSide::Buy,
-                },
+                }
+                .into(),
             ]
         );
 
-        orders = vec![
+        let mut orders: Vec<SellLimitOrder> = vec![
             LimitOrder {
                 trader_id: 1,
                 nonce: 2,
                 price: 2.0,
                 amount: 1,
-                side: OrderSide::Sell,
-            },
+            }
+            .into(),
             LimitOrder {
                 trader_id: 1,
                 nonce: 1,
                 price: 2.0,
                 amount: 1,
-                side: OrderSide::Sell,
-            },
+            }
+            .into(),
             LimitOrder {
                 trader_id: 1,
                 nonce: 3,
                 price: 1.0,
                 amount: 1,
-                side: OrderSide::Sell,
-            },
+            }
+            .into(),
         ];
         orders.sort();
+
         assert_eq!(
             orders.as_slice(),
             &[
@@ -318,22 +390,22 @@ pub mod tests {
                     nonce: 3,
                     price: 1.0,
                     amount: 1,
-                    side: OrderSide::Sell,
-                },
+                }
+                .into(),
                 LimitOrder {
                     trader_id: 1,
                     nonce: 1,
                     price: 2.0,
                     amount: 1,
-                    side: OrderSide::Sell,
-                },
+                }
+                .into(),
                 LimitOrder {
                     trader_id: 1,
                     nonce: 2,
                     price: 2.0,
                     amount: 1,
-                    side: OrderSide::Sell,
-                },
+                }
+                .into(),
             ]
         );
     }
@@ -367,13 +439,15 @@ pub mod tests {
         assert!(lob.buys.is_empty());
         assert_eq!(
             lob.sells.front(),
-            Some(&LimitOrder {
-                trader_id: seller_id,
-                price: 1_f32,
-                amount: 100,
-                nonce: 6,
-                side: OrderSide::Sell,
-            })
+            Some(
+                &LimitOrder {
+                    trader_id: seller_id,
+                    price: 1_f32,
+                    amount: 100,
+                    nonce: 6,
+                }
+                .into()
+            )
         );
     }
 
@@ -407,13 +481,15 @@ pub mod tests {
         assert!(lob.sells.is_empty());
         assert_eq!(
             lob.buys.front(),
-            Some(&LimitOrder {
-                trader_id: buyer_id,
-                price: 5_f32,
-                amount: 100,
-                nonce: 6,
-                side: OrderSide::Buy,
-            })
+            Some(
+                &LimitOrder {
+                    trader_id: buyer_id,
+                    price: 5_f32,
+                    amount: 100,
+                    nonce: 6,
+                }
+                .into()
+            )
         );
     }
 
@@ -431,13 +507,15 @@ pub mod tests {
 
         assert_eq!(
             lob.buys.front(),
-            Some(&LimitOrder {
-                trader_id: 2,
-                price: 4.0,
-                amount: 100,
-                nonce: 1,
-                side: OrderSide::Buy,
-            })
+            Some(
+                &LimitOrder {
+                    trader_id: 2,
+                    price: 4.0,
+                    amount: 100,
+                    nonce: 1,
+                }
+                .into()
+            )
         );
     }
 
@@ -455,13 +533,15 @@ pub mod tests {
 
         assert_eq!(
             lob.sells.front(),
-            Some(&LimitOrder {
-                trader_id: 2,
-                price: 5.0,
-                amount: 100,
-                nonce: 1,
-                side: OrderSide::Sell,
-            })
+            Some(
+                &LimitOrder {
+                    trader_id: 2,
+                    price: 5.0,
+                    amount: 100,
+                    nonce: 1,
+                }
+                .into()
+            )
         );
     }
 }
